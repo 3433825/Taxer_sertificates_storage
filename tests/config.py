fc771c8283f1002cee86ff2caf45aac9165bb956
oleksii <shapovalov.olexiy@gmail.com>
2026-03-25 21:22:53 +0200

    додані змінні CERT_VALID, CERT_EXPIRED в YAML-файл в config.py для цих змінних додані шляхи за замовчанням

--- tests/config.py
+++ tests/config.py
@@ -7,15 +7,13 @@ load_dotenv()
 class Config:
     BASE_URL = os.getenv("BASE_URL")
     # Динамічно отримуємо абсолютні шляхи до .cer файлів
-    CERT_VALID = os.path.abspath(os.getenv("CERT_VALID"))
-    CERT_EXPIRED = os.path.abspath(os.getenv("CERT_EXPIRED"))
     CERT_LONG_NAME = os.path.abspath(os.getenv("CERT_LONG_NAME"))
     CERT_BROKEN = os.path.abspath(os.getenv("CERT_BROKEN"))
 
-    # Додаємо цей блок, якого не вистачає:
+    CERT_VALID = os.path.abspath(os.getenv("CERT_VALID") or "data/valid_cert.cer")
+    CERT_EXPIRED = os.path.abspath(os.getenv("CERT_EXPIRED") or "data/expired.cer")
+
     CERTS = {
-        "valid": {"path": os.path.abspath(os.getenv("CERT_VALID", "data/valid.cer"))},
-        "expired": {
-            "path": os.path.abspath(os.getenv("CERT_EXPIRED", "data/expired.cer"))
-        },
+        "valid": {"path": CERT_VALID, "owner": "Олег Андрійович"},
+        "expired": {"path": CERT_EXPIRED},
     }
