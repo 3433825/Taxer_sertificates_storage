1a0ce0458f57e2a47d705cffb2ae36f1dc965c68
oleksii <shapovalov.olexiy@gmail.com>
2026-03-28 18:33:26 +0200

    додано формування файла з логами на кожну дату тестування

--- utils/logger.py
+++ utils/logger.py
@@ -1,9 +1,32 @@
 import logging
+import os
+from datetime import datetime
 
 
 def get_logger(name):
     logger = logging.getLogger(name)
-    # Якщо обробники вже додані (наприклад, через pytest), не додаємо їх знову
+
+    # Якщо обробники вже додані, не додаємо їх знову
     if not logger.handlers:
         logger.setLevel(logging.INFO)
+
+        # Створюємо папку logs, якщо її немає
+        log_dir = "logs"
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+
+        # Формуємо ім'я файлу на основі поточної дати
+        log_filename = datetime.now().strftime("%Y-%m-%d.log")
+        log_path = os.path.join(log_dir, log_filename)
+
+        # Налаштування формату
+        formatter = logging.Formatter(
+            "%(asctime)s [%(levelname)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+        )
+
+        # Додаємо запис у файл
+        file_handler = logging.FileHandler(log_path, encoding="utf-8")
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
     return logger
