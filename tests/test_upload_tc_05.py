a65dead298b4d182bddd6fffd9a94a621744966f
oleksii <shapovalov.olexiy@gmail.com>
2026-03-30 22:52:51 +0300

    внесені зміни пов'язані з allure_reports test_upload_tc_05.py test_details_tc_18.py добавлена фікстура в conftest.py добавлений метод в main_page.py

--- tests/test_upload_tc_05.py
+++ tests/test_upload_tc_05.py
@@ -1,4 +1,4 @@
-import pytest
+import allure
 from playwright.sync_api import expect
 from pages.main_page import MainPage
 from pages.upload_page import UploadPage
@@ -9,6 +9,10 @@ from tests.config import Config
 logger = get_logger(__name__)
 
 
+@allure.feature("Завантаження сертифікатів")
+@allure.story("Успішне додавання .cer файлу")
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.issue("TC-05", "Додавання сертифікату через Drag-and-Drop")
 def test_successful_certificate_upload_tc05(page):
     """
     TC-05: Додавання сертифікату.
@@ -19,27 +23,27 @@ def test_successful_certificate_upload_tc05(page):
 
     main_page = MainPage(page)
     upload_page = UploadPage(page)
-
-    # 1. Відкриття панелі
-    main_page.open_upload_screen()
-    logger.info("Панель завантаження відкрита.")
-
-    # 2. Завантаження через JS-drop (обхід BUG-05)
-    valid_cert_path = Config.CERTS["valid"]["path"]
-    upload_page.upload_via_drop(valid_cert_path)
-
-    # 3. Повернення до списку
-    upload_page.return_to_main()
-
-    # 4. Верифікація результату
     expected_name = CertConstants.VALID_OWNER
-    logger.info(f"Очікуємо появу сертифіката: {expected_name}")
+    valid_cert_path = Config.CERTS["valid"]["path"]
 
-    target_cert = main_page.get_cert_by_name(expected_name)
+    with allure.step("1. Відкриття панелі завантаження"):
+        main_page.open_upload_screen()
+        logger.info("Панель завантаження відкрита.")
 
-    expect(
-        target_cert,
-        message=f"Сертифікат '{expected_name}' не знайдено в списку після завантаження",
-    ).to_be_visible(timeout=10000)
+    with allure.step(f"2. Завантаження сертифікату через JS-drop: {valid_cert_path}"):
+        # Обхід BUG-05 (відсутність кнопки 'Вибрати')
+        upload_page.upload_via_drop(valid_cert_path)
+
+    with allure.step("3. Повернення до головного екрану"):
+        upload_page.return_to_main()
+
+    with allure.step(f"4. Верифікація появи сертифіката: {expected_name}"):
+        logger.info(f"Очікуємо появу сертифіката: {expected_name}")
+        target_cert = main_page.get_cert_by_name(expected_name)
+
+        expect(
+            target_cert,
+            message=f"Сертифікат '{expected_name}' не знайдено в списку після завантаження",
+        ).to_be_visible(timeout=10000)
 
     logger.info("--- TC-05 ЗАВЕРШЕНО УСПІШНО ---")
